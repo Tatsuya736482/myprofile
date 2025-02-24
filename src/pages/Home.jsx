5bd2a9c436944825c054df456f34a54d077926d9
Tatsuya736482 <141567698+Tatsuya736482@users.noreply.github.com>
2025-02-24 23:47:37 +0900

    fix: update asset paths in asset-manifest.json and index.html for new JS bundle, remove background color in Home component, and add license file for new JS bundle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
 
   return (
     <AppProvider>
-      <Paper sx={{backgroundColor: "#f5f5f5" }}>
+      <Paper >
         <Box
           display="flex"
           flexDirection="column"
